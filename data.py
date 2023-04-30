e73307fc79a77c165aca1483b0b50f8f16737cb9
Joshua Slui <joshuaslui0203@gmail.com>
2023-04-30 15:39:17 +0200

    Create data.py

---
+++ data.py
@@ -0,0 +1,156 @@
+awoo = ["https://cdn.discordapp.com/attachments/715969701771083820/739429902402715748/unknown.gif",
+        "https://cdn.discordapp.com/attachments/726377118304305245/739430343962001468/unknown.gif",
+        "https://cdn.discordapp.com/attachments/726377118304305245/739430488091000863/unknown.gif",
+        "https://cdn.discordapp.com/attachments/726377118304305245/739430548895825930/unknown.gif",
+        "https://cdn.discordapp.com/attachments/726377118304305245/739430608673177650/unknown.gif",
+        "https://cdn.discordapp.com/attachments/726377118304305245/739430699672797324/unknown.gif",
+        "https://media.giphy.com/media/jPSjVZLPOGVa0/giphy.gif",
+        "https://media.giphy.com/media/lLoYlICw9DEY0/giphy.gif",
+        "https://media.giphy.com/media/OQI1KMVGJyniwVwnl3/giphy.gif",
+        "https://media.giphy.com/media/2nXYb1jf0FVRK/giphy.gif"]
+
+bellyrub = ["https://i.imgur.com/VnoVdts.gif",
+            "https://i.imgur.com/AGb1AKb.gif",
+            "https://i.imgur.com/PbYykOz.gif",
+            "https://i.imgur.com/xxx0Ykb.gif",
+            "https://media1.tenor.com/images/904ca94bb9335013cf8e1d57115de5c5/tenor.gif?itemid=13642623",
+            "https://media1.tenor.com/images/d13c4bcb8939ab000deab59bb46ec189/tenor.gif?itemid=18000915",
+            "https://media1.tenor.com/images/93b76d5e74f7b39dc4404aaa2a5c6fa4/tenor.gif?itemid=7325356"]
+
+blush = ["https://media1.tenor.com/images/68e8bca8e90cb37fdcc9cc798bbc13f5/tenor.gif?itemid=10296487",
+         "https://media1.tenor.com/images/2c5fc8253882a23b7eb0f3077cfcd290/tenor.gif?itemid=5971288",
+         "https://media1.tenor.com/images/369329091222b088ddb788b6196be261/tenor.gif?itemid=17315979",
+         "https://media1.tenor.com/images/969eafdd133b4a1d24cb9135b4bf6310/tenor.gif?itemid=14043564",
+         "https://media1.tenor.com/images/41bc1ff3064c7a5aa0979c2ddee32dc8/tenor.gif?itemid=9869912",
+         "https://media1.tenor.com/images/e8ad00d3190adf65434672c075a4c9e1/tenor.gif?itemid=16242697"]
+
+boop = ["https://media1.tenor.com/images/36d187af3d5e876ec0d8f11cabe1a8d4/tenor.gif?itemid=15225693",
+        "https://media1.tenor.com/images/ea37d5c281b7ca7c35175bdc854b0a5b/tenor.gif?itemid=11159084",
+        "https://media.tenor.com/images/3f364e971453b18a1ae2b5cc8d2d0613/tenor.gif",
+        "https://media1.tenor.com/images/a0f0c6b3ef95bb2ff530a92d6c516cbd/tenor.gif?itemid=14452125",
+        "https://media1.tenor.com/images/5c0b2e9661b5ea40834fc6bd6b84a806/tenor.gif?itemid=14796547",
+        "https://media.tenor.com/images/aa41ce7ea8f138eb8e0be635cd4664d9/tenor.gif",
+        "https://media1.tenor.com/images/9e42f5059137a153137af77cb898c070/tenor.gif?itemid=17599907"]
+
+cuddle = ["https://thumbs.gfycat.com/WaterloggedFabulousIbis-small.gif",
+          "https://media3.giphy.com/media/h4BprYiFYNxRe/giphy.gif",
+          "https://media1.tenor.com/images/304d8bf0315c3096603afd7e6562833c/tenor.gif?itemid=12304006",
+          "https://media1.tenor.com/images/64ffd0c97258e49fd08f17397792c6c0/tenor.gif?itemid=5581481",
+          "https://media.tenor.com/images/5c02ca01064b6c2f75c6e1dbbbc5de90/tenor.gif",
+          "https://media1.tenor.com/images/e1035f7a2efb7722abd665f4e6a164be/tenor.gif?itemid=16022685",
+          "https://media1.tenor.com/images/2259bbd5696244422b64fc4c7d235a93/tenor.gif?itemid=13765442",
+          "https://media1.tenor.com/images/9d4d148f45d355cda83ada31b3112c85/tenor.gif?itemid=15681585"]
+
+feed = ["https://media1.tenor.com/images/5dda2ba3a73de6e8a3abfc45996ed1ba/tenor.gif?itemid=5879575",
+        "https://media1.tenor.com/images/9a861ec0dcdae5d505afbeca28910182/tenor.gif?itemid=6101207",
+        "https://media1.tenor.com/images/d598225fd7e2ba8bd82793856c78b19f/tenor.gif?itemid=4909807",
+        "https://media1.tenor.com/images/9337abd3a4247c73c218ea3c3662137d/tenor.gif?itemid=7266798",
+        "https://media.tenor.com/images/49109825019cb719d771ff54876fe078/tenor.gif"]
+
+glomp = ["https://media.tenor.com/images/2e04038ac35f6e7975f64cd2ba1c2d48/tenor.gif"]
+
+happy = ["https://media1.tenor.com/images/4b5d8967536d7235f5231ba9123964ab/tenor.gif?itemid=8675372",
+         "https://media.tenor.com/images/6a4617fb4777558efe818e2fa241c5fb/tenor.gif",
+         "https://media.tenor.com/images/7559368ede131c2f119cd19a4a6f7cf5/tenor.gif",
+         "https://media.tenor.com/images/f7ba3433bb89a484ec92d33b87f54ca7/tenor.gif",
+         "https://media.tenor.com/images/8784dfd64f06f19e1a25cb88f990b2b7/tenor.gif",
+         "https://media1.tenor.com/images/aca1a8cd3d8d7bf7d5703bf3f8117a4b/tenor.gif?itemid=5567628",
+         "https://media1.tenor.com/images/ab69ea03faa81a44af51c86770745f61/tenor.gif?itemid=9273180",
+         "https://media1.tenor.com/images/685f1055a8a4dbfe3318e5898d19b97c/tenor.gif?itemid=10253595",
+         "https://media1.tenor.com/images/41df672a1055e4f833e2022dce9231fd/tenor.gif?itemid=16660700",
+         "https://d.facdn.net/art/bluemachine/1573314972/1573314972.bluemachine_2.gif",
+         "https://media.tenor.com/images/a6fc8a84da7b76176d5b79d56c90ffb3/tenor.gif"]
+
+highfive = ["https://media1.tenor.com/images/bbc34d4c1409ddf777186c773334e3e6/tenor.gif?itemid=6123039",
+            "https://media.tenor.com/images/975d05f75a11fca62e2bb0b1ee040772/tenor.gif",
+            "https://media.tenor.com/images/be4e523990540bb9ad18ac49439e5cd2/tenor.gif",
+            "https://media.tenor.com/images/56eaf6892f8dd40c5f2b4580e8cd34e8/tenor.gif",
+            "https://media1.tenor.com/images/88ece6b5cf8b9df04c6290c5100a8410/tenor.gif?itemid=7367090",
+            "https://media1.tenor.com/images/ce04ff64e42ac6a0fc23dc1e1da79a2c/tenor.gif?itemid=14400946",
+            "https://media1.tenor.com/images/6e5ab91c3133061f67a1843df9ace592/tenor.gif?itemid=5442827",
+            "https://media1.tenor.com/images/59e5bbd7e22503afa1b0ec8176acaae9/tenor.gif?itemid=3431650"]
+
+hug = ["https://media1.tenor.com/images/faf29ddc23059c5740751a3fecc2f303/tenor.gif?itemid=13533690",
+       "https://media1.tenor.com/images/f20151a1f7e003426ca7f406b6f76c82/tenor.gif?itemid=13985247",
+       "https://media1.tenor.com/images/8ac5ada8524d767b77d3d54239773e48/tenor.gif?itemid=16334628",
+       "https://media1.tenor.com/images/68f16d787c2dfbf23a4783d4d048c78f/tenor.gif?itemid=9512793",
+       "https://media1.tenor.com/images/3fee00811a33590e4ee490942f233c78/tenor.gif?itemid=14712845",
+       "https://media1.tenor.com/images/2d13ede25b31d946284eaa3b8a4e6b31/tenor.gif?itemid=11990658",
+       "https://media1.tenor.com/images/37df0fae36f9cce061c3cec84fc97a08/tenor.gif?itemid=17781844",
+       "https://media1.tenor.com/images/ece75ca15b715aacd86724ee23604569/tenor.gif?itemid=16796068",
+       "https://media2.giphy.com/media/gl8ymnpv4Sqha/giphy.gif"]
+
+lick = ["https://i.kym-cdn.com/photos/images/original/001/099/490/e82.gif",
+        "https://media1.tenor.com/images/1b96b02b98756a1a45fdde87dacbb36b/tenor.gif?itemid=17522184",
+        "https://media.giphy.com/media/8v9njaWnlcW2c/giphy.gif",
+        "https://media1.tenor.com/images/f40e1b6271581699aed3c12d042c129c/tenor.gif?itemid=15743713",
+        "https://media1.tenor.com/images/cd4f8199d49badf7b90277f8b13f5186/tenor.gif?itemid=12816950",
+        "https://media1.tenor.com/images/cd45350c528cc577cbe0eb754e6e8241/tenor.gif?itemid=12181805"]
+
+pet = ["https://media1.tenor.com/images/1786de6797fd096ff503d633254786cc/tenor.gif?itemid=14598566",
+       "https://media1.tenor.com/images/be0c22e0af951aa7fa8753381663eb2c/tenor.gif?itemid=15824856",
+       "https://media1.tenor.com/images/fd7365b518ee53285f39ef8b1caf5b6e/tenor.gif?itemid=15523978",
+       "https://media1.tenor.com/images/adeb267fbe4ab5ddaa080297e5b2d0e4/tenor.gif?itemid=15740749",
+       "https://media1.tenor.com/images/62d861f1f0b6277e2056e86f4ba42e6f/tenor.gif?itemid=16276220"]
+
+rawr = ["https://media1.tenor.com/images/5393afcde44d4be4569108e40fddb3a1/tenor.gif?itemid=15945972",
+        "https://media1.tenor.com/images/0ffb715776443e6242b736420ce84144/tenor.gif?itemid=14558168",
+        "https://media1.tenor.com/images/6f5c4dd655d8d93b5f85d2f739f11cea/tenor.gif?itemid=16986104",
+        "https://media1.tenor.com/images/78e79fa52e3934f693ce670a3a905e64/tenor.gif?itemid=16549140"
+        "https://media1.tenor.com/images/84b80c7e1fa843fd4ea7b90e1bc269c4/tenor.gif?itemid=15426315"]
+
+smooch = ["https://media1.tenor.com/images/473c30ffa595a67a281d9b2fde7b48d6/tenor.gif?itemid=5373130",
+          "https://media.tenor.com/images/78aaf08e4cb6174a5d3a0d2826c43953/tenor.gif",
+          "https://media1.tenor.com/images/3fce8ce3fb358cc6fafd0517c6558653/tenor.gif?itemid=14802231",
+          "https://cdn.weasyl.com/~recurrent/submissions/1797089/3e4439c6a3b09383c2361d2db9f5d72f1e74f70dfaf26654b468875ee1d0071d/recurrent-c-kiss-under-the-moon.gif",
+          "https://pa1.narvii.com/6929/e06bf443286d81e79374a4374cac98a1e263ebb4r1-2000-2000_hq.gif",
+          "https://i.kym-cdn.com/photos/images/original/001/099/490/e82.gif",
+          "https://media1.tenor.com/images/12ff73f58a57174d6c6228f323e41c21/tenor.gif?itemid=11770367",
+          "https://d.facdn.net/art/kusuguttai/1550707158/1550707158.kusuguttai_kissandnibble.gif",
+          "https://i.imgur.com/ApV7HF5.gif",
+          "https://i.pinimg.com/originals/a2/64/72/a26472dd131d20dba037b81c1a633c61.gif",
+          "https://media1.tenor.com/images/5c8972db3df932240bb191be36c71b85/tenor.gif?itemid=7189564"]
+
+snuggle = ["https://media1.tenor.com/images/ba7829e33a9392d38e15cdc4c650b897/tenor.gif?itemid=17325139",
+           "https://media.giphy.com/media/kz01wlAoCLYx3v8F68/giphy.gif",
+           "https://media.giphy.com/media/DAX8RixOTrWuY/giphy.gif"]
+
+wag = ["https://media.tenor.com/images/391f32ea35c6bd010fe29425f8a689e3/tenor.gif",
+       "https://media.tenor.com/images/bac4a23e4a961a274c94d2631cf63e19/tenor.gif",
+       "https://media1.tenor.com/images/3562622d1c072ba2c30645b19e13bd2f/tenor.gif?itemid=7910058",
+       "https://media.tenor.com/images/4a53db6c0267f84b180d7b9d3c34f120/tenor.gif",
+       "https://media.tenor.com/images/dd137df1f2badd31405967a12d862f28/tenor.gif",
+       "https://media.tenor.com/images/7bd999202fde3cb64395725964ff34ac/tenor.gif"]
+
+cookie = ['https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-ms-fields-cookies-024-1544735513.jpg',
+          'https://images-gmi-pmc.edge-generalmills.com/087d17eb-500e-4b26-abd1-4f9ffa96a2c6.jpg',
+          'https://www.janespatisserie.com/wp-content/uploads/2020/04/IMG_0744_1-scaled.jpg',
+          'https://images.squarespace-cdn.com/content/v1/549367d9e4b0b0a3e2543543/1588847414826-FQZTCRKH9B7DBCHWPX04/ke17ZwdGBToddI8pDm48kGBZtRQfXGGrtPZLnhyM-_cUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYxCRW4BPu10St3TBAUQYVKc2A05hr64wuCTS4zP7CpzbvVdV_gnisg_uG0ZwZ1tao3WChTLVZ4KTQ-wgmrDxiyo/image-asset.jpeg']
+
+nuzzle = ['https://media1.tenor.com/images/412ed5e4675f2efa000882e933f1ffee/tenor.gif?itemid=12530414']
+
+paw = ["https://cdn.discordapp.com/attachments/1092442686671290378/1092442923158736966/IMG_1802.png",
+       "https://cdn.discordapp.com/attachments/1092442686671290378/1092442937473908976/IMG_1822.png",
+       "https://cdn.discordapp.com/attachments/1092442686671290378/1097869824991953008/Paw_Boop.png",
+       "https://cdn.discordapp.com/attachments/1092442686671290378/1097869825214263426/Paw_Hug_Me.png",
+       "https://cdn.discordapp.com/attachments/1092442686671290378/1097869825449148548/Paw_OWO.png",
+       "https://cdn.discordapp.com/attachments/1092442686671290378/1097869825696596098/Paw_Yes.png",
+       "https://cdn.discordapp.com/attachments/1092442686671290378/1097869825948270752/Paw_Evil.png",
+       "https://cdn.discordapp.com/attachments/1092442686671290378/1097869826342518895/Paw_Look.png",
+       "https://cdn.discordapp.com/attachments/1092442686671290378/1097869826652909598/Paw_Kisses.png",
+       "https://cdn.discordapp.com/attachments/1092442686671290378/1097869826904571964/Paw_No.png"
+       ]
+
+dance = ["https://media.tenor.com/OxrPk4Zbf1IAAAAC/dancing-protogen.gif",
+         "https://media.tenor.com/pTxxtlWeKBMAAAAC/furry-dance.gif",
+         "https://media.tenor.com/EtShnq3WAWkAAAAd/eevee-furry.gif",
+         "https://media.tenor.com/5FlpxgiZWe4AAAAd/keenora-kee.gif",
+         "https://media.tenor.com/3U0EPDQsvx4AAAAd/furry-furries.gif"]
+
+colors = ["Red", "Green", "Blue", "Pink", "Purple", "Brown", "Black", "White", "Orange", "Teal", "Light Green",
+          "Light Blue", "Grey", "Yellow"]
+
+species = {"Dragon": [80, 300], "Fox": [35, 50], "Deer": [53, 120], "Wolf": [60, 90], "Dog": [15, 110],
+           "Bunny": [25, 35], "Protogen": [60, 120], "Hyena": [75, 105], "Tiger": [75 - 107], "Lion": [110, 140],
+           "Bird": [5, 50], "Otter": [20, 30], "Snake": [5, 20], "Cat": [15, 30], "Sergal": [80, 120],
+           "Horse": [110, 150], "Shark": [40, 60], "Lizard": [10 - 30]}
