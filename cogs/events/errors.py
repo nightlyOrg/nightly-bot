3e33a6b525da48ba59625546d32c38d442511a0b
ToothyDev <chosenfate@gmx.de>
2023-04-30 18:12:07 +0200

    Convert error handler to slash commands

--- cogs/events/errors.py
+++ cogs/events/errors.py
@@ -10,31 +10,31 @@ class error(commands.Cog, name="Error"):
         self.bot = bot
 
     @commands.Cog.listener()
-    async def on_command_error(self, ctx, err):
+    async def on_application_command_error(self, ctx, err):
         if isinstance(err, commands.CommandNotFound):
             return
 
         if isinstance(err, commands.MissingPermissions):
             perms = "`" + '`, `'.join(err.missing_permissions) + "`"
-            return await ctx.send("{} **You are missing {} permissions.**".format(config.crossmark, perms))
+            return await ctx.respond("{} **You are missing {} permissions.**".format(config.crossmark, perms), ephemeral=True)
 
         if isinstance(err, commands.BotMissingPermissions):
             perms = "`" + '`, `'.join(err.missing_permissions) + "`"
-            return await ctx.send("{} **I'm missing {} permissions**".format(config.crossmark, perms))
+            return await ctx.respond("{} **I'm missing {} permissions**".format(config.crossmark, perms), ephemeral=True)
 
         if isinstance(err, commands.MissingRequiredArgument):
-            return await ctx.send("{} **`{}` is a required argument!**".format(config.crossmark, err.param.name))
+            return await ctx.respond("{} **`{}` is a required argument!**".format(config.crossmark, err.param.name), ephemeral=True)
 
         if isinstance(err, commands.CommandOnCooldown):
-            return await ctx.send(
+            return await ctx.respond(
                 "{} **This command is on cooldown for __{:.0f}__ more seconds.**".format(config.crossmark,
-                                                                                         err.retry_after))
+                                                                                         err.retry_after), ephemeral=True)
 
         if isinstance(err, commands.MemberNotFound):
-            return await ctx.send("{0} **Could not find user `{1}`**".format(config.confused, err.argument))
+            return await ctx.respond("{0} **Could not find user `{1}`**".format(config.confused, err.argument), ephemeral=True)
 
         if isinstance(err, discord.NotFound):
-            return await ctx.send("I could not find the argument you have provided.")
+            return await ctx.respond("I could not find the argument you have provided.", ephemeral=True)
 
 
 def setup(bot):
