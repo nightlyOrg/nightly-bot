eba9128f9b298bef049a83fee8bf5346af2612f4
ToothyDev <chosenfate@gmx.de>
2023-04-30 18:31:15 +0200

    Update error formatting

--- cogs/events/errors.py
+++ cogs/events/errors.py
@@ -16,22 +16,17 @@ class error(commands.Cog, name="Error"):
 
         if isinstance(err, commands.MissingPermissions):
             perms = "`" + '`, `'.join(err.missing_permissions) + "`"
-            return await ctx.respond("{} **You are missing {} permissions.**".format(config.crossmark, perms), ephemeral=True)
+            return await ctx.respond(f"{config.crossmark} **You are missing {perms} permissions.**", ephemeral=True)
 
         if isinstance(err, commands.BotMissingPermissions):
             perms = "`" + '`, `'.join(err.missing_permissions) + "`"
-            return await ctx.respond("{} **I'm missing {} permissions**".format(config.crossmark, perms), ephemeral=True)
-
-        if isinstance(err, commands.MissingRequiredArgument):
-            return await ctx.respond("{} **`{}` is a required argument!**".format(config.crossmark, err.param.name), ephemeral=True)
+            return await ctx.respond(f"{config.crossmark} **I'm missing {perms} permissions**", ephemeral=True)
 
         if isinstance(err, commands.CommandOnCooldown):
-            return await ctx.respond(
-                "{} **This command is on cooldown for __{:.0f}__ more seconds.**".format(config.crossmark,
-                                                                                         err.retry_after), ephemeral=True)
+            return await ctx.respond(f"{config.crossmark} **This command is on cooldown for {round(err.retry_after)} more seconds.**", ephemeral=True)
 
         if isinstance(err, commands.MemberNotFound):
-            return await ctx.respond("{0} **Could not find user `{1}`**".format(config.confused, err.argument), ephemeral=True)
+            return await ctx.respond(f"{config.confused} **Could not find user `{err.argument}`", ephemeral=True)
 
         if isinstance(err, discord.NotFound):
             return await ctx.respond("I could not find the argument you have provided.", ephemeral=True)
