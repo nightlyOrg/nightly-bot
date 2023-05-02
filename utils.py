fde818aef9864cdec92c1b1fed381b45885a4d8d
ToothyDev <chosenfate@gmx.de>
2023-05-03 01:17:50 +0200

    Prepare interactions() for API change

--- utils.py
+++ utils.py
@@ -11,7 +11,11 @@ class Colors:
 
 
 async def interactions(ctx, members, name, error_name, giflist, altname=None):
-    image = random.choice(giflist)
+    if isinstance(giflist, str):
+        json = await apireq(giflist)
+        image = json['link']
+    else:
+        image = random.choice(giflist)
     display_giflist = []
     for x in members:
         display_giflist.append(x.display_name)
@@ -51,7 +55,11 @@ class interactionsView(discord.ui.View):
         if len(self.members) == 0:
             self.disable_all_items()
             await interaction.message.edit(view=self)
-        image = random.choice(self.giflist)
+        if isinstance(self.giflist, str):
+            json = await apireq(self.giflist)
+            image = json['link']
+        else:
+            image = random.choice(self.giflist)
         embed = discord.Embed(
             description=f"**{interaction.user.name}** {self.name} **" + self.ctx.author.name + "** back!",
             color=discord.Color.blue())
