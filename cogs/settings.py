82eb27c15f1874f1903492e197032698fa9461cb
Joshua Slui <joshuaslui0203@gmail.com>
2023-05-03 19:23:23 +0200

    Im not sure what i changed here

--- cogs/settings.py
+++ cogs/settings.py
@@ -24,23 +24,23 @@ class Settings(commands.Cog, name="settings"):
             return await ctx.respond(result)
 
 
-        @settings.command()
-        @commands.cooldown(1, 5, commands.BucketType.user)
-        async def currency(self, ctx, enabled: discord.SlashCommandOptionType.boolean):
-            """ Disable or enable currency """
-            cursor = await utils.mysql_login()
-            database = cursor.cursor()
-            database.execute("SELECT config FROM settings WHERE GUILD = %s", [ctx.guild.id])
-            result = database.fetchall()[0][0]
-            result = json.loads(result)
-            result['currency'] = enabled
-            newConfig = json.dumps(result)
+    @settings.command()
+    @commands.cooldown(1, 5, commands.BucketType.user)
+    async def currency(self, ctx, enabled: discord.SlashCommandOptionType.boolean):
+        """ Disable or enable currency """
+        cursor = await utils.mysql_login()
+        database = cursor.cursor()
+        database.execute("SELECT config FROM settings WHERE GUILD = %s", [ctx.guild.id])
+        result = database.fetchall()[0][0]
+        result = json.loads(result)
+        result['currency'] = enabled
+        newConfig = json.dumps(result)
 
-            database.execute("UPDATE settings SET config = %s WHERE GUILD = %s", [newConfig, ctx.guild.id])
-            cursor.commit()
-            database.close()
-            cursor.close()
-            return await ctx.respond(f"Currency is now {'enabled' if result['currency'] else 'disabled'}.")
+        database.execute("UPDATE settings SET config = %s WHERE GUILD = %s", [newConfig, ctx.guild.id])
+        cursor.commit()
+        database.close()
+        cursor.close()
+        return await ctx.respond(f"Currency is now {'enabled' if result['currency'] else 'disabled'}.")
 
 def setup(bot):
     bot.add_cog(Settings(bot))
