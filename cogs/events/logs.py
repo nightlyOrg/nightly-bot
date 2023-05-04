2053f9f9d314f9dc29b6b71de81958340a97b772
Joshua Slui <joshuaslui0203@gmail.com>
2023-05-04 14:36:55 +0200

    fix newlines (pep8)

--- cogs/events/logs.py
+++ cogs/events/logs.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from discord.ext import commands
 from utils import mysql_login
 
+
 # from cogs.admin import admin_only
 
 class logs(commands.Cog, name="Logs"):
@@ -13,14 +14,17 @@ class logs(commands.Cog, name="Logs"):
     @commands.Cog.listener()
     async def on_application_command(self, ctx):
         if ctx.guild:
-            print(f"{datetime.now().__format__('%a %d %b %y, %H:%M:%S')} - {ctx.guild.name} | {ctx.author} > {ctx.command}")
+            print(
+                f"{datetime.now().__format__('%a %d %b %y, %H:%M:%S')} - {ctx.guild.name} | {ctx.author} > {ctx.command}")
         else:
-            print(f"{datetime.now().__format__('%a %d %b %y, %H:%M:%S')} - Direct Messages | {ctx.author} > {ctx.command}")
+            print(
+                f"{datetime.now().__format__('%a %d %b %y, %H:%M:%S')} - Direct Messages | {ctx.author} > {ctx.command}")
 
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
         print(f"{datetime.now().__format__('%a %d %b %y, %H:%M:%S')} - Added to {guild.name}")
-        print(f"{datetime.now().__format__('%a %d %b %y, %H:%M:%S')} - Loading default settings and inserting it into database")
+        print(
+            f"{datetime.now().__format__('%a %d %b %y, %H:%M:%S')} - Loading default settings and inserting it into database")
 
         configuration = {'currency': True, 'socials': True}
         configuration = json.dumps(configuration)
@@ -33,16 +37,19 @@ class logs(commands.Cog, name="Logs"):
             database.close()
             cursor.close()
         except Exception as error:
-            print(f"{datetime.now().__format__('%a %d %b %y, %H:%M:%S')} - Received fatal connection | Could not complete operation")
+            print(
+                f"{datetime.now().__format__('%a %d %b %y, %H:%M:%S')} - Received fatal connection | Could not complete operation")
             traceback.print_tb(error.__traceback__)
 
         else:
-            print(f"{datetime.now().__format__('%a %d %b %y, %H:%M:%S')} - Successfully loaded and inserted default settings")
+            print(
+                f"{datetime.now().__format__('%a %d %b %y, %H:%M:%S')} - Successfully loaded and inserted default settings")
 
     @commands.Cog.listener()
     async def on_guild_remove(self, guild):
         print(f"{datetime.now().__format__('%a %d %b %y, %H:%M:%S')} - Removed from {guild.name}")
-        print(f"{datetime.now().__format__('%a %d %b %y, %H:%M:%S')} - Deleting configuration settings from database...")
+        print(
+            f"{datetime.now().__format__('%a %d %b %y, %H:%M:%S')} - Deleting configuration settings from database...")
 
         try:
             cursor = await mysql_login()
@@ -52,11 +59,14 @@ class logs(commands.Cog, name="Logs"):
             database.close()
             cursor.close()
         except Exception as error:
-            print(f"{datetime.now().__format__('%a %d %b %y, %H:%M:%S')} - Received fatal connection | Could not complete operation")
+            print(
+                f"{datetime.now().__format__('%a %d %b %y, %H:%M:%S')} - Received fatal connection | Could not complete operation")
             traceback.print_tb(error.__traceback__)
 
         else:
-            print(f"{datetime.now().__format__('%a %d %b %y, %H:%M:%S')} - Successfully deleted configuration settings from database")
+            print(
+                f"{datetime.now().__format__('%a %d %b %y, %H:%M:%S')} - Successfully deleted configuration settings from database")
+
 
 def setup(bot):
     bot.add_cog(logs(bot))
