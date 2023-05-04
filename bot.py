4993a215bd4c3bfcf82ec79069c3d02b87b1ed8c
ToothyDev <chosenfate@gmx.de>
2023-05-04 17:57:38 +0200

    Change bot to be slash-only instance

--- bot.py
+++ bot.py
@@ -6,9 +6,8 @@ from config import token
 from utils import mysql_login
 
 intents = Intents(guilds=True, guild_messages=True)
-# intents.message_content = True #Uncomment this if you use prefixed command that are not mentions
-bot = bridge.Bot(intents=intents, command_prefix=">>", status=Status.dnd,
-                 activity=Activity(type=ActivityType.watching, name="you"))
+bot = discord.Bot(intents=intents, status=Status.dnd,
+                  activity=Activity(type=ActivityType.watching, name="you"))
 
 
 class MyNewHelp(commands.MinimalHelpCommand):
