df52b7e1445777353aed48627b3671d986df640c
Joshua Slui <joshuaslui0203@gmail.com>
2023-05-05 19:16:09 +0200

    Add data

---
+++ utilities/data.py
@@ -0,0 +1,10 @@
+class Colors:
+    blue = 0xadd8e6
+    red = 0xf04747
+    green = 0x90ee90
+    orange = 0xfaa61a
+
+
+class Emotes:
+    bankCard = '<a:MoneyCard2:1103307033941385247>'
+    cash = '<:cash:1103307142410272768>'
