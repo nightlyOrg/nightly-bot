27d0576716adb27ee7a948cfb71b5117bf06ea08
ToothyDev <chosenfate@gmx.de>
2023-05-05 23:02:24 +0200

    Add banner, emoji & sticker counts

--- cogs/utility.py
+++ cogs/utility.py
@@ -14,12 +14,16 @@ class Utility(commands.Cog, name="utility"):
         owner = await guild.fetch_member(guild.owner_id)
         embed = discord.Embed(color=Colors.blue, title=guild.name)
         embed.set_thumbnail(url=guild.icon.url)
-        embed.add_field(name="Owner", value=owner.mention)
+        embed.add_field(name="Owner", value=owner.mention, inline=True)
         embed.add_field(name="Members", value=guild.member_count, inline=True)
         embed.add_field(name="Roles", value=len(await guild.fetch_roles()), inline=True)
-        embed.add_field(name="Channels", value=f"{len(guild.text_channels)} Text, {len(guild.voice_channels)} Voice")
-        embed.add_field(name="Created", value=f"<t:{round(guild.created_at.timestamp())}:R>")
+        embed.add_field(name="Channels", value=f"{len(guild.text_channels)} Text, {len(guild.voice_channels)} Voice", inline=True)
+        embed.add_field(name="Created", value=f"<t:{round(guild.created_at.timestamp())}:R>", inline=True)
+        embed.add_field(name="Emojis", value=len(guild.emojis), inline=True)
+        embed.add_field(name="Stickers", value=len(guild.stickers), inline=True)
         embed.set_footer(text=f"ID: {guild.id}")
+        if (guild.banner):
+            embed.set_image(url=guild.banner.url)
         await ctx.respond(embed=embed)
 
 
