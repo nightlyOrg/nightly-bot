05758b260c554089abb354d1072c4f2f5178c545
Joshua Slui <joshuaslui0203@gmail.com>
2023-05-05 19:15:47 +0200

    Use database functions

--- cogs/currency.py
+++ cogs/currency.py
@@ -13,17 +13,12 @@ class Currency(commands.Cog, name="currency"):
     @commands.cooldown(1, 5, commands.BucketType.user)
     async def balance(self, ctx):
         """ Check your balance """
-        await utils.checkSettingsValue(ctx, 'currency')
-        cursor = await utils.mysql_login()
-        database = cursor.cursor()
-        database.execute("SELECT cash, bank FROM economy WHERE UID = %s", [ctx.author.id])
-        result = database.fetchall()
-        database.close()
+        embed = discord.Embed(colour=Colors.blue)
 
-        embed = discord.Embed(colour=utils.Colors.blue)
+        result = await selector("SELECT cash, bank FROM economy WHERE UID = %s", [ctx.author.id])
 
         if not result:
-            result = [(0, 0)]
+            result = (0, 0)
             embed.add_field(name="WARNING",
                             value="***Please make sure you agree to our [privacy policy](https://github.com/MiataBoy) before continuing.***\nThis warning will disappear when you earn money.")
 
