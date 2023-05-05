eb03ebe0dc4488024323c4684a1a42f41a1b2838
ToothyDev <chosenfate@gmx.de>
2023-05-06 00:55:02 +0200

    Use description instead of fields

--- cogs/utility.py
+++ cogs/utility.py
@@ -1,7 +1,7 @@
 import discord
 from discord import slash_command
 from discord.ext import commands
-from utilities.data import Colors
+import random
 
 
 class Utility(commands.Cog, name="utility"):
@@ -12,15 +12,17 @@ class Utility(commands.Cog, name="utility"):
     async def serverinfo(self, ctx: discord.ApplicationContext):
         guild = ctx.guild
         owner = await guild.fetch_member(guild.owner_id)
-        embed = discord.Embed(color=Colors.blue, title=guild.name)
+        embed = discord.Embed(color=discord.Color.random(), title=guild.name)
+        embed.description = f"""
+**Owner:** {owner.mention}
+**Members:** {guild.member_count}
+**Roles:** {len(await guild.fetch_roles())}
+**Channels:** {len(guild.text_channels)} Text, {len(guild.voice_channels)} Voice
+**Created:** <t:{round(guild.created_at.timestamp())}:R>
+**Emojis:** {len(guild.emojis)}
+**Stickers:** {len(guild.stickers)}
+        """
         embed.set_thumbnail(url=guild.icon.url)
-        embed.add_field(name="Owner", value=owner.mention, inline=True)
-        embed.add_field(name="Members", value=guild.member_count, inline=True)
-        embed.add_field(name="Roles", value=len(await guild.fetch_roles()), inline=True)
-        embed.add_field(name="Channels", value=f"{len(guild.text_channels)} Text, {len(guild.voice_channels)} Voice", inline=True)
-        embed.add_field(name="Created", value=f"<t:{round(guild.created_at.timestamp())}:R>", inline=True)
-        embed.add_field(name="Emojis", value=len(guild.emojis), inline=True)
-        embed.add_field(name="Stickers", value=len(guild.stickers), inline=True)
         embed.set_footer(text=f"ID: {guild.id}")
         if (guild.banner):
             embed.set_image(url=guild.banner.url)
