e7eeeb350dc18d8954923cf1ef39314cea191644
Joshua Slui <joshuaslui0203@gmail.com>
2023-05-05 19:24:42 +0200

    use Emotes class

--- cogs/events/errors.py
+++ cogs/events/errors.py
@@ -16,20 +16,20 @@ class error(commands.Cog, name="Error"):
 
         if isinstance(err, commands.MissingPermissions):
             perms = "`" + '`, `'.join(err.missing_permissions) + "`"
-            return await ctx.respond(f"{config.crossmark} **You are missing {perms} permissions.**", ephemeral=True)
+            return await ctx.respond(f"{Emotes.crossmark} **You are missing {perms} permissions.**", ephemeral=True)
 
         if isinstance(err, commands.BotMissingPermissions):
             perms = "`" + '`, `'.join(err.missing_permissions) + "`"
-            return await ctx.respond(f"{config.crossmark} **I'm missing {perms} permissions**", ephemeral=True)
+            return await ctx.respond(f"{Emotes.crossmark} **I'm missing {perms} permissions**", ephemeral=True)
 
         if isinstance(err, commands.CommandOnCooldown):
-            return await ctx.respond(f"{config.crossmark} **This command is on cooldown for {round(err.retry_after)} more seconds.**", ephemeral=True)
+            return await ctx.respond(f"{Emotes.crossmark} **This command is on cooldown for {round(err.retry_after)} more seconds.**", ephemeral=True)
 
         if isinstance(err, commands.MemberNotFound):
-            return await ctx.respond(f"{config.confused} **Could not find user `{err.argument}`", ephemeral=True)
+            return await ctx.respond(f"{Emotes.confused} **Could not find user `{err.argument}`", ephemeral=True)
 
         if isinstance(err, discord.NotFound):
-            return await ctx.respond("I could not find the argument you have provided.", ephemeral=True)
+            return await ctx.respond(f"{Emotes.confused} I could not find the argument you have provided.", ephemeral=True)
 
 
 def setup(bot):
