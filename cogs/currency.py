5e00b37db1d0b05550d5b8b1f99738e11ebe2a69
Joshua Slui <joshuaslui0203@gmail.com>
2023-05-06 17:15:18 +0200

    Remove weird formatting

--- cogs/currency.py
+++ cogs/currency.py
@@ -20,8 +20,7 @@ class Currency(commands.Cog, name="currency"):
 
         if not result:
             result = (0, 0)
-            embed.add_field(name="WARNING",
-                            value="***Please make sure you agree to our [privacy policy](https://github.com/MiataBoy) before continuing.***\nThis warning will disappear when you earn money.")
+            embed.add_field(name="WARNING", value="***Please make sure you agree to our [privacy policy](https://github.com/MiataBoy) before continuing.***\nThis warning will disappear when you earn money.")
 
         embed.description = f"You have {Emotes.cash} `{f'{result[0]:_}'.replace('_', '.')}` in your wallet\nYou have {Emotes.bankCard} `{f'{result[1]:_}'.replace('_', '.')}` in your bank"
 
