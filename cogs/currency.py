08bf6910bfcabfa7bc82b2a89a2bd06094363087
Joshua Slui <joshuaslui0203@gmail.com>
2023-05-06 17:15:31 +0200

    Add daily command

--- cogs/currency.py
+++ cogs/currency.py
@@ -26,6 +26,17 @@ class Currency(commands.Cog, name="currency"):
 
         return await ctx.respond(embed=embed, ephemeral=True)
 
+    @slash_command(brief="Check your balance")
+    @commands.cooldown(1, 5, commands.BucketType.user)
+    async def daily(self, ctx):
+        cooldownStatus = await checkCooldown(ctx)
+        if cooldownStatus is not True:
+            return await ctx.respond(f'Sorry, but you still have to wait till <t:{cooldownStatus}:f>')
+        await createCooldown(ctx, 24)
+        dailyAmount = random.randint(300, 500)
+        await modifyData("INSERT INTO economy (UID,CASH, BANK) VALUES(%s, %s, %s) ON DUPLICATE KEY UPDATE CASH = CASH + %s", [ctx.author.id, dailyAmount, 0, dailyAmount])
+        return await ctx.respond(f'Congratulations! You got {dailyAmount}.')
+
 
 def setup(bot):
     bot.add_cog(Currency(bot))
