6e71e305175a86a4a9d540be674c0b7db4b250de
Joshua Slui <joshuaslui0203@gmail.com>
2023-05-06 22:08:49 +0200

    Add links

--- utilities/data.py
+++ utilities/data.py
@@ -12,5 +12,10 @@ class Emotes:
     checkmark = '<:Check:909435999216623636>'
     confused = '<a:loading:908629364378325023>'
 
+
 class Links:
     discord = 'https://discord.gg/qWyNwSx2rx'
+    invite = 'https://discord.com/api/oauth2/authorize?client_id=1102234738594418739&permissions=412384349248&scope=bot%20applications.commands'
+    github = 'https://github.com/NightlyOrg/Nightly-bot'
+    privacy = 'https://github.com/nightlyOrg/nightly-bot/wiki/privacy'
+    terms = 'https://github.com/nightlyOrg/nightly-bot/wiki/terms'
