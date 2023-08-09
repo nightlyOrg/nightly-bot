2e05751c56fd6f808729a17efe611403a8e14024
Joshua Slui <joshuaslui0203@gmail.com>
2023-08-09 20:49:48 +0200

    Add the [0] back..?

--- bot.py
+++ bot.py
@@ -43,7 +43,7 @@ async def on_ready():
 
 @bot.check
 async def block_disabled_commands(ctx):
-    result = (await selector("SELECT config FROM settings WHERE GUILD = %s", [ctx.guild.id]))
+    result = (await selector("SELECT config FROM settings WHERE GUILD = %s", [ctx.guild.id]))[0]
     result = json.loads(result)
 
     cog = ctx.cog.__class__.__name__
